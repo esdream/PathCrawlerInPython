e1de7cddebab714989e25c649efb8b2cb947f659
Zucky FU <conquerfu@gmail.com>
2017-10-26 10:44:11 +0800

    'change-error-file-path-to-data-directory'

--- path_crawler/conf/global_settings.py
+++ path_crawler/conf/global_settings.py
@@ -16,6 +16,10 @@ CITY_COMS_URL = GLOBAL_SETTINGS_URL + '/../data/city_coms/'
 
 PATH_DATA_URL = GLOBAL_SETTINGS_URL + '/../data/path_data/'
 
+CRAWL_ERROR_URL = GLOBAL_SETTINGS_URL + '/../data/crawl_error/'
+
+PARSE_ERROR_URL = GLOBAL_SETTINGS_URL + '/../data/parse_error/'
+
 if(__name__ == '__main__'):
     print(CITY_COMS_URL)
     print(CITIES_URL)
