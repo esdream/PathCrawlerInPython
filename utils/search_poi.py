81e2ca16cd1132e6a1b12407f9515250ce48f883
Zucky FU <conquerfu@gmail.com>
2017-12-13 11:33:58 +0800

    search-poi-finished'

--- utils/search_poi.py
+++ utils/search_poi.py
@@ -15,6 +15,11 @@ import requests
 
 from path_crawler.conf import global_settings
 
+SEARCH_QUEUE = queue.Queue()
+POI_QUEUE = queue.Queue(300)
+ERROR_LOCK = threading.Lock()
+PARSER_EXIT_FLAG = False
+
 class SearchReader(object):
     def __init__(self, search_file, search_queue):
         self._search_file = search_file
@@ -32,7 +37,7 @@ class SearchReader(object):
             for row in csv_search:
                 self._search_queue.put(row)
 
-
+# 关键字搜索
 class KeywordSearcher(threading.Thread):
 
     def __init__(self, **crawl_args):
@@ -47,7 +52,7 @@ class KeywordSearcher(threading.Thread):
         self._search_poi()
         print('No.{} KeywordSearcher finished!'.format(self._thread_id))
 
-    # 关键字查找的源数据格式：id,keywords(多个关键字用竖线|分割),types(多个类型用竖线|分割),查询城市
+    # 关键字查找的源数据格式：id,keywords(多个关键字用竖线|分割),types(多个类型用竖线|分割),city(查询城市)
     def _search_poi(self):
         while True:
             if(self._search_queue.empty()):
@@ -85,10 +90,6 @@ class KeywordSearcher(threading.Thread):
                                     self._poi_queue.put(poi)
                                     print('id:{0}, page num{1} crawl succeed!'.format(poi['id'], page_num))
                                     
-                                    # 检查_poi_queue中元素个数
-                                    print(self._poi_queue.qsize())
-
-
                                     time.sleep(random.random())
 
                                     break
@@ -104,6 +105,147 @@ class KeywordSearcher(threading.Thread):
                             print(crawl_error)
                             self._search_queue.put(search_req)
 
+
+# 周边搜索
+class AroundSearcher(threading.Thread):
+
+    def __init__(self, **crawl_args):
+        threading.Thread.__init__(self)
+        self._thread_id = crawl_args['thread_id']
+        self._search_queue = crawl_args['search_queue']
+        self._poi_queue = crawl_args['poi_queue']
+        self._search_params = crawl_args['search_params']
+
+    def run(self):
+        print('No.{} AroundSearcher start...'.format(self._thread_id))
+        self._search_poi()
+        print('No.{} AroundSearcher finished!'.format(self._thread_id))
+
+    # 周边搜索的源数据格式：id,keywords(多个关键字用竖线|分割),types(多个类型用竖线|分割),city(查询城市),location(中心点坐标，经度和纬度用,分割，经度在前，纬度在后),radius(查找半径，取值范围0-50000m),sortrule(返回结果排序规则，按距离排序：distance，综合排序：weight)
+    def _search_poi(self):
+        while True:
+            if(self._search_queue.empty()):
+                break
+            else:
+
+                search_req = self._search_queue.get()
+
+                url = 'http://restapi.amap.com/v3/place/around?&keywords={0[1]}&types={0[2]}&city={0[3]}&location={0[4]}&radius={0[5]}&sortrule={0[6]}&output=json&offset=20&key={key}&extensions=all'.format(
+                    search_req, **self._search_params)
+
+                timeout = 2
+                while timeout > 0:
+                    timeout -= 1
+                    try:
+                        poi_res = requests.get(url, timeout=5).json()
+                        poi_count = int(poi_res[u'count'])
+                        pages = poi_count // 20 + 1
+
+                        for page in range(pages):
+                            while True:
+                                try:
+                                    if(self._poi_queue.full()):
+                                        time.sleep(8)
+                                    page_num = str(page + 1)
+                                    sub_url = url + '&page=' + page_num
+
+                                    poi = {}
+                                    poi['id'] = search_req[0]
+                                    poi['keywords'] = search_req[1]
+                                    poi['types'] = search_req[2]
+                                    poi['city'] = search_req[3]
+                                    poi['page_num'] = page_num
+                                    poi['info'] = requests.get(
+                                        sub_url, timeout=5).json()
+                                    self._poi_queue.put(poi)
+                                    print('id:{0}, page num{1} crawl succeed!'.format(
+                                        poi['id'], page_num))
+
+                                    time.sleep(random.random())
+
+                                    break
+                                except Exception as error:
+                                    print(error)
+                                    print('id:{0} page num {1} have some error, crawl again...'.format(
+                                        poi['id'],  page_num))
+
+                        break
+
+                    except Exception as crawl_error:
+                        if(timeout == 0):
+                            print(crawl_error)
+                            self._search_queue.put(search_req)
+
+
+# 多边形搜索
+class PolygonSearcher(threading.Thread):
+
+    def __init__(self, **crawl_args):
+        threading.Thread.__init__(self)
+        self._thread_id = crawl_args['thread_id']
+        self._search_queue = crawl_args['search_queue']
+        self._poi_queue = crawl_args['poi_queue']
+        self._search_params = crawl_args['search_params']
+
+    def run(self):
+        print('No.{} PolygonSearcher start...'.format(self._thread_id))
+        self._search_poi()
+        print('No.{} PolygonSearcher finished!'.format(self._thread_id))
+
+    # 多边形搜索的源数据格式：id,keywords(多个关键字用竖线|分割),types(多个类型用竖线|分割),city(城市),polygon(经纬度坐标对，经纬度用,分割，经度在前纬度在后，坐标对用|分割。多边形为矩形时可传入左上右下两顶点坐标对，其他情况下首尾坐标对需相同)
+    def _search_poi(self):
+        while True:
+            if(self._search_queue.empty()):
+                break
+            else:
+
+                search_req = self._search_queue.get()
+
+                url = 'http://restapi.amap.com/v3/place/polygon?&keywords={0[1]}&types={0[2]}&city={0[3]}&polygon={0[4]}&output=json&offset=20&key={key}&extensions=all'.format(
+                    search_req, **self._search_params)
+
+                timeout = 2
+                while timeout > 0:
+                    timeout -= 1
+                    try:
+                        poi_res = requests.get(url, timeout=5).json()
+                        poi_count = int(poi_res[u'count'])
+                        pages = poi_count // 20 + 1
+
+                        for page in range(pages):
+                            while True:
+                                try:
+                                    if(self._poi_queue.full()):
+                                        time.sleep(8)
+                                    page_num = str(page + 1)
+                                    sub_url = url + '&page=' + page_num
+
+                                    poi = {}
+                                    poi['id'] = search_req[0]
+                                    poi['keywords'] = search_req[1]
+                                    poi['types'] = search_req[2]
+                                    poi['city'] = search_req[3]
+                                    poi['page_num'] = page_num
+                                    poi['info'] = requests.get(
+                                        sub_url, timeout=5).json()
+                                    self._poi_queue.put(poi)
+                                    print('id:{0}, page num{1} crawl succeed!'.format(
+                                        poi['id'], page_num))
+
+                                    time.sleep(random.random())
+
+                                    break
+                                except Exception as error:
+                                    print(error)
+                                    print('id:{0} page num {1} have some error, crawl again...'.format(
+                                        poi['id'],  page_num))
+
+                        break
+
+                    except Exception as crawl_error:
+                        if(timeout == 0):
+                            print(crawl_error)
+                            self._search_queue.put(search_req)
 
 class PoiParser(threading.Thread):
 
@@ -127,10 +269,8 @@ class PoiParser(threading.Thread):
         while not PARSER_EXIT_FLAG:
 
             try:
-                #检查_poi_queue中元素个数
-                print(self._poi_queue.qsize())
 
-                poi = self._poi_queue.get(True, 20)
+                poi = self._poi_queue.get(True, 10)
                 
                 result = {}
                 result['id'] = poi['id']
@@ -147,11 +287,15 @@ class PoiParser(threading.Thread):
                     result['poi_type'] = poi['info'][u'pois'][i][u'type']
                     result['poi_typecode'] = poi['info'][u'pois'][i][u'typecode']
                     result['poi_address'] = poi['info'][u'pois'][i][u'address']
+                    if(not result['poi_address']):
+                        result['poi_address'] = 'null'
                     result['poi_loc'] = poi['info'][u'pois'][i][u'location']
 
                     result_vector = (
                         result['id'], result['keywords'], result['types'], result['city'], result['page_num'], result['poi_id'], result['poi_name'], result['poi_type'], result['poi_typecode'], result['poi_address'], result['poi_loc'])
+
                     self._data_batch.append(result_vector)
+
 
                 if(len(self._data_batch) > 200):
                     poi_db = sqlite3.connect(self._poi_file)
@@ -161,21 +305,15 @@ class PoiParser(threading.Thread):
                         self._data_batch[:] = []
 
                 self._poi_queue.task_done()
-                break
 
             except Exception as parse_error:
+                print('Parse id {0} page {1} failed!'.format(poi['id'], poi['page_num']))
+                print(parse_error)
                 with self._error_lock:
                     self._error_file.write(
                         '{id},{keywords},{types},{city}\n'.format(**poi))
-                    print('Parse path {} failed!'.format(
-                        poi['id']))
-                    print(parse_error)
 
 
-SEARCH_QUEUE = queue.Queue()
-POI_QUEUE = queue.Queue(300)
-ERROR_LOCK = threading.Lock()
-PARSER_EXIT_FLAG = False
 
 def main():
 
@@ -200,55 +338,83 @@ def main():
     poi_db = sqlite3.connect(poi_file)
     with poi_db:
         poi_db.execute(
-            'create table poi(id int, keywords varchar(255), types varchar(255), city varchar(20), page_num int, poi_id varchar(20), poi_name varchar(20), poi_type varchar(50), poi_typecode int, poi_address varchar(255), poi_loc varchar(20))')
+            'create table poi(id int, keywords varchar(255), types varchar(255), city varchar(20), page_num varchar(20), poi_id varchar(20), poi_name nvarchar(20), poi_type varchar(50), poi_typecode varchar(20), poi_address nvarchar(255), poi_loc varchar(20))')
 
-    with open(parse_error_file, mode='w', encoding='utf-8') as f_pares_error:
-        if(int(search_type) == 1):
-            search_threads = []
-            search_list = ['search_thread' + str(num) for num in range(2)]
-            for search_thread_id in search_list:
-                search_thread = KeywordSearcher(
-                    thread_id=search_thread_id,
-                    search_queue=SEARCH_QUEUE,
-                    poi_queue=POI_QUEUE,
-                    search_params=search_params
-                )
-                search_thread.start()
-                search_threads.append(search_thread)
+        with open(parse_error_file, mode='w', encoding='utf-8') as f_pares_error:
 
-        data_batch = []
-        parse_thread_id = 'parse_thread'
-        poi_parser = PoiParser(
-            thread_id=parse_thread_id,
-            poi_file=poi_file,
-            poi_queue=POI_QUEUE,
-            error_file=f_pares_error,
-            error_lock=ERROR_LOCK,
-            data_batch=data_batch
-        )
-        poi_parser.start()
+            if(int(search_type) == 1):
+                search_threads = []
+                search_list = ['search_thread' + str(num) for num in range(5)]
+                for search_thread_id in search_list:
+                    search_thread = KeywordSearcher(
+                        thread_id=search_thread_id,
+                        search_queue=SEARCH_QUEUE,
+                        poi_queue=POI_QUEUE,
+                        search_params=search_params
+                    )
+                    search_thread.start()
+                    search_threads.append(search_thread)
 
-        while not SEARCH_QUEUE.empty():
-            pass
+            if(int(search_type) == 2):
+                search_threads = []
+                search_list = ['search_thread' + str(num) for num in range(5)]
+                for search_thread_id in search_list:
+                    search_thread = AroundSearcher(
+                        thread_id=search_thread_id,
+                        search_queue=SEARCH_QUEUE,
+                        poi_queue=POI_QUEUE,
+                        search_params=search_params
+                    )
+                    search_thread.start()
+                    search_threads.append(search_thread)
 
-        for search_thread in search_threads:
-            search_thread.join()
+            if(int(search_type) == 3):
+                search_threads = []
+                search_list = ['search_thread' + str(num) for num in range(5)]
+                for search_thread_id in search_list:
+                    search_thread = PolygonSearcher(
+                        thread_id=search_thread_id,
+                        search_queue=SEARCH_QUEUE,
+                        poi_queue=POI_QUEUE,
+                        search_params=search_params
+                    )
+                    search_thread.start()
+                    search_threads.append(search_thread)
 
-        while not POI_QUEUE.empty():
-            pass
-        
-        global PARSER_EXIT_FLAG
-        PARSER_EXIT_FLAG = True
 
-        poi_parser.join()
+            data_batch = []
+            parse_thread_id = 'parse_thread'
+            poi_parser = PoiParser(
+                thread_id=parse_thread_id,
+                poi_file=poi_file,
+                poi_queue=POI_QUEUE,
+                error_file=f_pares_error,
+                error_lock=ERROR_LOCK,
+                data_batch=data_batch       
+            )
+            poi_parser.start()
 
-        poi_db.executemany('insert into poi values (?,?,?,?,?,?,?,?,?,?,?)', data_batch)
-        poi_db.commit()
-        data_batch[:] = []
+            while not SEARCH_QUEUE.empty():
+                pass
 
-        print('Exiting Main Thread')
-        with ERROR_LOCK:
-            f_pares_error.close()
+            for search_thread in search_threads:
+                search_thread.join()
+
+            while not POI_QUEUE.empty():
+                pass
+            
+            global PARSER_EXIT_FLAG
+            PARSER_EXIT_FLAG = True
+
+            poi_parser.join()
+
+            poi_db.executemany('insert into poi values (?,?,?,?,?,?,?,?,?,?,?)', data_batch)
+            poi_db.commit()
+            data_batch[:] = []
+
+            print('Exiting Main Thread')
+            with ERROR_LOCK:
+                f_pares_error.close()
 
 if(__name__ == '__main__'):
     main()
